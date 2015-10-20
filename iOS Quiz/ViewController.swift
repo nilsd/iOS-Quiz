723c6e526b97b9d01753e3305add24ccaf62de7a
nilsd <nils@dunso.se>
2015-10-20 17:38:29 +0200

    Initial Commit

---
+++ iOS Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Quiz
+//
+//  Created by Nils Dunsö on 2015-10-20.
+//  Copyright © 2015 Dunso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
